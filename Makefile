aa10d502433fa32d8186ac074bbe945fe10e55e7
Raise-NCCR <raise1229@gmail.com>
2021-12-01 12:48:55 +0900

    値によってビット長の変わる符号化アルゴリズム

--- Makefile
+++ Makefile
@@ -1,11 +1,24 @@
-all: clean encode decode
-	./encode && ./decode
+binary: binary-encode binary-decode
+	./binary_encode && ./binary_decode
+
+binary-all: clean binary
+
+binary-encode:
+	gcc -g binary_encode.c -o binary_encode.out
+
+binary-decode:
+	gcc -g binary_decode.c -o binary_decode.out
+
+normal: encode decode
+	./encode.out && ./decode.out
+
+normal-all: clean normal
 
 encode:
-	gcc -g encode.c -o encode
+	gcc -g encode.c -o encode.out
 
 decode:
-	gcc -g decode.c -o decode
+	gcc -g decode.c -o decode.out
 
 clean:
-	rm encode decode encoded decoded.wav
+	rm *.out *decoded.wav *encoded
--- binary_encode.c
+++ binary_encode.c
@@ -8,9 +8,11 @@ int main(int argc, char *argv[])
     unsigned char chunk[44];
     size_t data_length = 10100;
     size_t data_idx = 0;
+    size_t encoded_data_idx = 0;
     size_t input_length;
     unsigned char input_data[50000];
-    unsigned char *data = calloc(data_length, 90000);
+    unsigned char *data = calloc(data_length, 1000000);
+    unsigned char *encoded_data = calloc(data_length, 100000);
     fread(chunk, 1, 44, input_fp);
     fwrite(chunk, 1, 44, output_fp);
     while (input_length = fread(input_data, 1, 10000, input_fp))
@@ -19,11 +21,22 @@ int main(int argc, char *argv[])
         {
             for (unsigned char binary_length = 0; binary_length < input_data[i]; binary_length++)
             {
-                data[binary_length] = 1;
+                data[data_idx + binary_length] = 1;
             }
-            data[input_data[i]] = 0;
-            fwrite(data, 1, input_data[i] + 1, output_fp);
+            data[data_idx + input_data[i]] = 0;
+            data_idx += input_data[i] + 1;
         }
     }
+    for (int i = 0; i < data_idx; i++)
+    {
+        unsigned char num = 0;
+        for (int j = 0; j < 8; j++)
+        {
+            num = num * 2 + data[i + j];
+        }
+        encoded_data[encoded_data_idx++] = num;
+        i += 7;
+    }
+    fwrite(encoded_data, 1, encoded_data_idx, output_fp);
     return 0;
 }
